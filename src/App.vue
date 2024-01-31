062f696af686dda62b206aa9bdbd5fcb88951f0e
TrThao <thaotranit.hubt@gmail.com>
2024-01-31 09:52:02 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <main-screen v-if="statusMatch === 'Default'" @onStart="onHandlBeforeStart" />
+  <interact-screen
+    v-if="statusMatch === 'Match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'Default'"
+  />
+  <copy-right />
+</template>
+
+<script>
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+import CopyRight from "./components/CopyRight.vue";
+import { shuffled } from "./utils/array";
+
+export default {
+  name: "App",
+  components: {
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+    CopyRight,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "Default",
+    };
+  },
+
+  methods: {
+    onHandlBeforeStart(config) {
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+
+      this.settings.cardsContext = shuffled(shuffled(shuffled(cards)));
+      this.settings.startedAt = new Date().getTime();
+      this.statusMatch = "Match";
+    },
+    onGetResult() {
+      this.statusMatch = "result";
+      this.timer = new Date().getTime() - this.settings.startedAt;
+    },
+  },
+};
+</script>
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copyright a {
+  color: #f4dc26;
+}
+</style>
